34abba7ce2e278306254da7d021921aee9b3fe10
hajimekaneko <hajime.10600@gmail.com>
2021-11-06 17:44:13 +0900

    axios......

--- myappsite/frontend/src/store/actions.js
+++ myappsite/frontend/src/store/actions.js
@@ -12,28 +12,28 @@ export default {
       .catch(err => { throw err })
   },
 
-  fetchLists: ({ commit }) => {
-    // TODO:
-    throw new Error('fetchLists action should be implemented')
-  },
+  // fetchLists: ({ commit }) => {
+  //   // TODO:
+  //   throw new Error('fetchLists action should be implemented')
+  // },
 
-  addTask: ({ commit }) => {
-    // TODO:
-    throw new Error('addTask action should be implemented')
-  },
+  // addTask: ({ commit }) => {
+  //   // TODO:
+  //   throw new Error('addTask action should be implemented')
+  // },
 
-  updateTask: ({ commit }) => {
-    // TODO:
-    throw new Error('updateTask action should be implemented')
-  },
+  // updateTask: ({ commit }) => {
+  //   // TODO:
+  //   throw new Error('updateTask action should be implemented')
+  // },
 
-  removeTask: ({ commit }) => {
-    // TODO:
-    throw new Error('removeTask action should be implemented')
-  },
+  // removeTask: ({ commit }) => {
+  //   // TODO:
+  //   throw new Error('removeTask action should be implemented')
+  // },
 
-  logout: ({ commit }) => {
-    // TODO:
-    throw new Error('logout action should be implemented')
-  }
+  // logout: ({ commit }) => {
+  //   // TODO:
+  //   throw new Error('logout action should be implemented')
+  // }
 }
--- myappsite/frontend/src/store/mutations.js
+++ myappsite/frontend/src/store/mutations.js
@@ -5,28 +5,28 @@ export default {
     state.auth = payload
   },
 
-  [types.FETCH_ALL_TASKLIST] (state, payload) {
-    // TODO:
-    throw new Error('FETCH_ALL_TASKLIST mutation should be implemented')
-  },
+  // [types.FETCH_ALL_TASKLIST] (state, payload) {
+  //   // TODO:
+  //   throw new Error('FETCH_ALL_TASKLIST mutation should be implemented')
+  // },
 
-  [types.ADD_TASK] (state, payload) {
-    // TODO:
-    throw new Error('ADD_TASK mutation should be implemented')
-  },
+  // [types.ADD_TASK] (state, payload) {
+  //   // TODO:
+  //   throw new Error('ADD_TASK mutation should be implemented')
+  // },
 
-  [types.UPDATE_TASK] (state, payload) {
-    // TODO:
-    throw new Error('UPDATE_TASK mutation should be implemented')
-  },
+  // [types.UPDATE_TASK] (state, payload) {
+  //   // TODO:
+  //   throw new Error('UPDATE_TASK mutation should be implemented')
+  // },
 
-  [types.REMOVE_TASK] (state, payload) {
-    // TODO:
-    throw new Error('REMOVE_TASK mutation should be implemented')
-  },
+  // [types.REMOVE_TASK] (state, payload) {
+  //   // TODO:
+  //   throw new Error('REMOVE_TASK mutation should be implemented')
+  // },
 
-  [types.AUTH_LOGOUT] (state) {
-    // TODO:
-    throw new Error('AUTH_LOGOUT mutation should be implemented')
-  }
+  // [types.AUTH_LOGOUT] (state) {
+  //   // TODO:
+  //   throw new Error('AUTH_LOGOUT mutation should be implemented')
+  // }
 }
